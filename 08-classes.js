31566892d97fc56d39fc34a66b7dc969c109565b
Alvaro Maceda <alvaro.maceda@ceedcv.es>
2021-11-11 16:47:42 +0100

    working...

---
+++ 08-classes.js
@@ -0,0 +1,12 @@
+
+
+
+console.log('----------------------------------------------')
+console.log("Classes can be created dynamically")
+console.log('----------------------------------------------')
+
+let MyClass = class {
+  sayHello() {
+    console.log('Hello!');
+  }
+};
